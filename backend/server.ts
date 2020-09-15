775f654e3ebd817dce9e31f12cf5a7f65e7ee915
jmdevelopr <kuba.makuch28@gmail.com>
2020-09-15 18:48:41 +0200

    git pages testing

--- backend/server.ts
+++ backend/server.ts
@@ -5,32 +5,20 @@ import path from "path";
 import webpack from "webpack"; 
 import config from "../webpack.config";
 
-import cors from 'cors';
 import mongoose from 'mongoose';
 
 import cookieParser from "cookie-parser";
 
 import postRoutes from './routes/index';
-import { verifyJWT } from './middleware/authMiddleware';
 
 import env from "./env";
 
 const compiler = webpack(config);  
 const app: Application = express();
 
-//app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use(postRoutes);
-//app.use(express.static('../index.html'));
-
-// app.use(express.static('../dist/bundle.js'));
-// app.use(express.static('./dist/bundle.js'));
-// app.use(express.static('./bundle.js'));
-
-// app.use(express.static(path.join(__dirname, '..', '/dist', 'bundle.js')));
-// app.use(express.static(path.join(__dirname, '/dist', 'bundle.js')));
-// app.use(express.static(path.join(__dirname, 'bundle.js')));
 
 const dbURI = `mongodb+srv://${env.MONGO_USER}:${env.MONGO_PASS}@ratsdb.nhfz4.mongodb.net/${env.MONGO_DB}?retryWrites=true&w=majority`;
 const options = { useNewUrlParser: true, useUnifiedTopology: true };
@@ -44,11 +32,7 @@ app.use(require('webpack-dev-middleware')(compiler, {
 
 app.get('/*', (req: Request, res: Response) => {
     res.sendFile(path.join(__dirname, '..', '/index.html'))
-})
-
-app.get('/api', (req: Request, res: Response) => {  
-    res.send('<p>This is a api Data</p>');  
-});  
+}) 
 
 mongoose.connect(dbURI, options)
     .then(() => {
